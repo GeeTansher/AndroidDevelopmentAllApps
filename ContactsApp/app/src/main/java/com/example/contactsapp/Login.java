54aad51f4458be765d7ac652dc1dc810c4bd21a1
Geetansh Verma <vrgpverma@gmail.com>
2022-01-17 01:01:01 +0530

    changes

---
+++ ContactsApp/app/src/main/java/com/example/contactsapp/Login.java
@@ -0,0 +1,73 @@
+package com.example.contactsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+
+    private View pblogin;
+    private View lvform;
+    private TextView tvload;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        pblogin=findViewById(R.id.pblogin);
+        lvform=findViewById(R.id.lvform);
+        tvload =findViewById(R.id.tvload);
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            lvform.setVisibility(show ? View.GONE : View.VISIBLE);
+            lvform.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    lvform.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            pblogin.setVisibility(show ? View.VISIBLE : View.GONE);
+            pblogin.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    pblogin.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+
+            tvload.setVisibility(show ? View.VISIBLE : View.GONE);
+            tvload.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    tvload.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            pblogin.setVisibility(show ? View.VISIBLE : View.GONE);
+            tvload.setVisibility(show ? View.VISIBLE : View.GONE);
+            lvform.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
+}
---
+++ ContactsApp/app/src/main/java/com/example/contactsapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.contactsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.backendless.Backendless;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.exceptions.BackendlessFault;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
